06ccfb98f95bfee5b30d352f9326711bbc87e94c
DavidGinovyan25 <david48952@gmail.com>
2025-10-08 00:28:39 +0300

    delete build

--- build/src/ArgParser/CMakeFiles/argparser.dir/ArgParser.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/ArgParser/CMakeFiles/argparser.dir/ArgParser.cpp.o: \
- /home/ginovada/c++/ArgParser/src/ArgParser/ArgParser.cpp \
- /usr/include/stdc-predef.h \
- /home/ginovada/c++/ArgParser/include/ArgumentParser.hpp

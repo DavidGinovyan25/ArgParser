db987fe3512570110ff1fd7196f268ea04a5f37e
DavidGinovyan25 <david48952@gmail.com>
2025-11-04 18:44:28 +0300

    ref(IsReversedCommand): CheckCommand -> IsReversedCommand

--- include/ArgumentParser.hpp
+++ include/ArgumentParser.hpp
@@ -6,8 +6,10 @@ namespace ArgumentParser {
 enum class ContainerType {
     INT_T,
     STRING_T,
-    FLAG_T
+    FLAG_T,
+    HELP_T
 };
+
 struct Command {
     int min_args_count = 0;
     ContainerType type;
@@ -21,57 +23,21 @@ using CommandsContainer = std::vector<Command>;
 
 class CommandHandler {
 public:
-    // CommandsContainer int_commands;      
-    // CommandsContainer string_commands;
-    // CommandsContainer flag_commands;
-    // CommandsContainer help_commands;
     CommandsContainer commands;
-    bool CheckCommand(const char param1);
-    bool CheckCommand(const std::string& param1);
     bool IsCommand(const char* flag, const char* string);
+    bool IsReservedCommand(const std::string& s);
 };
 }
 
 namespace ArgumentParser {
-inline bool CommandHandler::CheckCommand(const char param1) {
-    for (int i = 0; i < commands.size(); ++i) {
-        if (param1 == commands[i].param1)
-            return true;
-    }
-    // for (int i = 0; i < int_commands.size(); ++i) {
-    //     if (param1 == int_commands[i].param1)
-    //         return true;
-    // }
-    // for (int i = 0; i < string_commands.size(); ++i) {
-    //     if (param1 == string_commands[i].param1)
-    //         return true;
-    // }
-    // for (int i = 0; i < flag_commands.size(); ++i) {
-    //     if (param1 == flag_commands[i].param1)
-    //         return true;
-    // }
-    return false;
-}
-inline bool CommandHandler::CheckCommand(const std::string& param2) {
-    for (int i = 0; i < commands.size(); ++i) {
-        if (param2 == commands[i].param2)
-            return true;
-    }
-    // for (int i = 0; i < int_commands.size(); ++i) {
-    //     if (param2 == int_commands[i].param2)
-    //         return true;
-    // }
-    // for (int i = 0; i < string_commands.size(); ++i) {
-    //     if (param2 == string_commands[i].param2)
-    //         return true;
-    // }
-    // for (int i = 0; i < flag_commands.size(); ++i) {
-    //     if (param2 == flag_commands[i].param2)
-    //         return true;
-    // }
-    return false;
-}
 inline bool CommandHandler::IsCommand(const char* flag, const char* string) {
     return (std::strncmp(flag, string, strlen(string)) == 0) ? true : false;
+}
+inline bool CommandHandler::IsReservedCommand(const std::string& s) {
+    for (Command cmd : commands) {
+        if ((s.size() > 2 && s.substr(2) == cmd.param2) || (s.size() > 1 && s[1] == cmd.param1))                             
+            return true;
+    }
+    return false;
 }
 }

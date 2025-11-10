a4ecb8f207e09812ba64e85655d5cb90f1660f9b
DavidGinovyan25 <david48952@gmail.com>
2025-11-11 01:55:02 +0300

    feat: create 2 container for commans

--- include/ArgumentParser.hpp
+++ include/ArgumentParser.hpp
@@ -18,15 +18,17 @@ struct Command {
     std::string description;
     std::vector<std::string> args;
     int min_args_count = 0;
-    bool miltivalue = false;
+    bool multivalue = false;
     bool positional = false;
 };
 
-using CommandsContainer = std::unordered_map<std::string, Command>;
+using CommandsDictionary = std::unordered_map<std::string, Command>;
+using CommandsOrder = std::vector<Command>;
 
 class CommandHandler {
 public:
-    CommandsContainer commands;
+    CommandsOrder commands_list;
+    CommandsDictionary commands_dict;
     bool IsCommand(const char* flag, const char* string);
     bool IsReservedCommand(const std::string& s);
 };
@@ -37,7 +39,7 @@ inline bool CommandHandler::IsCommand(const char* flag, const char* string) {
     return (std::strncmp(flag, string, strlen(string)) == 0) ? true : false;
 }
 inline bool CommandHandler::IsReservedCommand(const std::string& s) {
-    for (const auto& [key, cmd] : commands) {
+    for (const auto& [key, cmd] : commands_dict) {
         if ((s.size() > 2 && s.substr(2) == cmd.param2) || (s.size() > 1 && s[1] == cmd.param1))                             
             return true;
     }
